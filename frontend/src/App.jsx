8a8224469276fd3f64413bec274d3e91f3a42d33
synergisticgreed <sameergptnsr@gmail.com>
2025-03-31 12:33:08 +0530

    tailwind config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='text-3xl text-center text-blue-500'>
+      <h1 className='text-5xl'>Hello World</h1>
+      Tailwind Test
     </div>
   )
 }
